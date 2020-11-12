ef61df80ea0853970263247476ab725ce30d8a96
maximchuk35 <61186003+maximchuk35@users.noreply.github.com>
2020-11-12 11:57:09 +0200

    Identity робить

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Voice.Data;
+
+[assembly: HostingStartup(typeof(Voice.Areas.Identity.IdentityHostingStartup))]
+namespace Voice.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
